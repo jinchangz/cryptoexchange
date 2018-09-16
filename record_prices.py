4aa137bf415a24b824b9daecd0e4814693f1886e
Masato Hosokawa <masatohosokawa@Masatos-Mac.local>
2018-09-16 18:20:34 +0900

    [del]wasted files and print script

--- record_prices.py
+++ record_prices.py
@@ -40,25 +40,11 @@ if __name__ == '__main__':
         for con in cons:
             exchanges = []
             filename = os.path.normpath(os.path.join(os.path.abspath('./'),'prices/' + con + '_' + today + '.csv'))
-            print(filename)
+
             for ex in exs:
                 exchanges.append(get_exchange(ex, con))
 
-
-        # target_line = linecache.getline(filename, 1)
-        # inlines = target_line.split(',')
-        # # print(len(inlines))
-        # if inlines[0].replace('.','').isnumeric() or len(inlines) < 2:
-        #     if not(os.path.isfile(filename)):
-        #         with open(filename, mode='w') as f:
-        #             f.write('')
-        #     print('write ex_pairs in headline')
-        #     with open(filename, mode='r+') as f:
-        #         f.write(','.join(list(map(str, exs)))+'\n')
-
             prices = ','.join(list(map(str, exchanges)))
-                # print(prices)
-
             cmd = "echo " + prices + " >> " + filename
             subprocess.run(cmd, shell=True)
 
