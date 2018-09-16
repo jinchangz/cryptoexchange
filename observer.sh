5c6b984c4efad9a9c62d963a56beff54ced03bab
Masato Hosokawa <masatohosokawa@Masatos-Mac.local>
2018-09-16 14:02:19 +0900

    [mv]get_ec_ticker.py /get_tickers/get_ex_ticker.py

---
+++ observer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+processName="shell.py"
+restartCmd="python shell.py &"
+interval=10
+
+array=(`cat participant.txt|xargs`) 
+echo ${array[1]}
+while true
+do
+    for v in "${array[@]}"
+    do
+        isAlive=`ps -ef | grep "$v" | grep -v grep | wc -l`
+        if [ $isAlive = 1 ]; then
+            echo "Server is running."
+        else
+            echo "Server is dead, restarting..."
+            python $v &
+        fi
+    done
+    sleep $interval
+done
